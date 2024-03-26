d750a8087ca234b724f3f0f33e493576baedfcf7
Alejandro Azcona <alejandro@monaverse.com>
2024-03-26 15:35:52 -0600

    Adding Vector Variable related enums

---
+++ Runtime/Core/State/Enums/VectorAxisEnums.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mona.SDK.Core.State.Structs
+{
+    public enum VectorTwoAxis
+    {
+        X = 0,
+        Y = 1,
+        Z = 2
+    }
+
+    public enum VectorThreeAxis
+    {
+        X = 0,
+        Y = 1,
+        Z = 2
+    }
+}
