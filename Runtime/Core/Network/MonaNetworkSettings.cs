2d734c10fbc47aa790b4f31e16f9fd6c1768edc7
Ryan Green <ryan@numinousgames.com>
2024-06-14 20:09:02 -0600

    default network to none

--- Runtime/Core/Network/MonaNetworkSettings.cs
+++ Runtime/Core/Network/MonaNetworkSettings.cs
@@ -10,7 +10,7 @@ namespace Mona.SDK.Core.Network
     [Serializable]
     public class MonaNetworkSettings : IMonaNetworkSettings
     {
-        public MonaNetworkType _NetworkType = MonaNetworkType.Shared;
+        public MonaNetworkType _NetworkType = MonaNetworkType.None;
         public MonaNetworkType NetworkType { get => _NetworkType; set => _NetworkType = value; }
 
         public MonaNetworkType GetNetworkType() => NetworkType;
