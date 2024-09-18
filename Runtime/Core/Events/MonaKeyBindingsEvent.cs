5e7f1165ba32a3ea02fcbba5ddf620edf1315a73
Ryan Green <ryan.green@monaverse.com>
2024-09-18 17:44:26 -0600

    key bindings core event

---
+++ Runtime/Core/Events/MonaKeyBindingsEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Mona.SDK.Core.Events
+{
+    public struct MonaKeyBindingsEvent
+    {
+        public bool Enable;
+        public MonaKeyBindingsEvent(bool enable)
+        {
+            Enable = enable;
+        }
+    }
+}
